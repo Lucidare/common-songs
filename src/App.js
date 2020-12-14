e755386af54a02db3b06275a89db6c9a7ccdadd8
Brian Au <brian.au1996@gmail.com>
2020-12-13 20:55:05 -0500

    Added temp login for t oken

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
 import './App.css';
 
 function App() {
   // const [state, setstate] = useState(initialState)
 
+  useEffect(() => {
+    let token = new URLSearchParams(window.location.search).get('access_token')
+    console.log(token);
+  }, []);
+
   return (
     <Container className="container">
       <Row className="center">
         <h1 className="title">Common Songs</h1>
         <p className="subtitle">Find what songs you have in common</p>
-        <Button className="logInBtn" onClick={() => console.log("button clicked")} color="">LOG IN WITH SPOTIFY</Button>
+        <Button className="logInBtn" onClick={() => window.location = 'http://localhost:8888/login'} color="">LOG IN WITH SPOTIFY</Button>
       </Row>
     </Container>
   );
