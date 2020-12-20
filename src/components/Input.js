c0d96b937fc713a8a6bbbddaaf0e64034de44d6e
Brian Au <brian.au1996@gmail.com>
2020-12-20 12:35:21 -0500

    updated input text

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,7 +7,7 @@ export default function Input(props) {
     const validateInput = props.validateInput;
     return (  
         <div>
-        <p className="text">Enter a username, playlist or user link/URI</p>
+        <p className="text">Enter a username, Spotify URI or link to a user/playlist</p>
         <input name="input" value={input} onChange={e => setInput(e.target.value)}/>
         <div className="spaced">
           <Button className="greenBtn" onClick={validateInput}>Find Common Songs</Button>
